1505c698cf31df4b847a73a69fe665d4b2067efe
宮平しょうた <2525.natuta@gmail.com>
2015-05-10 21:51:47 +0900

    記事追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,57 @@
-var gulp = require('gulp');
-var gulputil = require('gulp-util');
-var sass = require('gulp-sass');
-var connect = require('gulp-connect');
-var minify = require('gulp-minify-css');
-var autoprefixer = require('gulp-autoprefixer');
-var uglify = require('gulp-uglify');
-var plumber = require('gulp-plumber');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer")
+var uglify = require("gulp-uglify");
+var browser = require("browser-sync");
+var plumber = require("gulp-plumber");
 
-var jade = require('gulp-jade')
-/*
-var imagemin = require('gulp-imagemin');
-var pngcrush = require('imagemin-pngcrush');*/
-
-// Jadeビルドタスク
-gulp.task('jade', function () {
-  gulp.src(['./jade/*.jade', '!./jade/temp_*.jade'])
-    .pipe(jade({pretty:true}))
-    .pipe(gulp.dest('./'))
+// gulp監視
+gulp.task("default", function() {
+    gulp.watch(['*.html'],['html']);    //htmlファイルを監視
+	gulp.watch(['./assets/scss/*.scss'],['sass']); //scssファイルを監視
+	gulp.watch(['./assets/css/*.css'],['html']); //cssファイルを監視
+	gulp.watch(['./assets/edit_js/*.js'],['js_compress']); //jsファイルを監視
 });
 
-// Sass(SCSS)ビルドタスク
-gulp.task('sass', function () { 
-    // Sass(SCSS)読み込み用ディレクトリ指定
-    gulp.src('./assets/scss/*.scss')
-    //エラー時再起させる
-    .pipe(plumber())
-    //コンパイルメソッド実行 
-    .pipe(sass())
-	//prefixer
-	.pipe(autoprefixer({
-		browsers: ['last 1 versions', "ie 9"],
-		cascade: false
-    }))
-	//minify
-	.pipe(minify({}))
-    //コンパイル後出力ファイル先の指定
-    .pipe(gulp.dest('./assets/css')); 
+//　自動更新
+gulp.task("server", function() {
+    browser({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
 
 //htmlタスク
 gulp.task('html',function(){
   gulp.src('./*.html')          //実行するファイル
-    .pipe(connect.reload());    //ブラウザの更新
+    .pipe(plumber())
+    .pipe(gulp.dest("./*.html"))
+    .pipe(browser.reload({stream:true}))
 });
 
-// JavaScirpt ミニファイ
-gulp.task('js_compress', function() {
-  gulp.src('./assets/edit_js/*.js')
-    .pipe(uglify({mangle: false}))
-    .pipe(gulp.dest('./assets/js'))
+gulp.task("js", function() {
+    gulp.src(["./assets/js/*.js","!./assets/js/min/*.js"])
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(gulp.dest("./assets/js/min"))
+        .pipe(browser.reload({stream:true}))
 });
  
-//ファイルの監視
-gulp.task('watch',function(){
-  gulp.watch(['./*.html'],['html']);    //htmlファイルを監視
-  gulp.watch(['./assets/scss/*.scss'],['sass']); //scssファイルを監視
-  gulp.watch(['./assets/css/*.css'],['html']); //cssファイルを監視
-  gulp.watch(['./assets/edit_js/*.js'],['js_compress']); //jsファイルを監視
-  gulp.watch(['./jade/*.jade'],['jade']); //jsファイルを監視
-});
-
-//ローカルサーバー
-gulp.task('connectDev',function(){
-  connect.server({
-    root: ['./'],   //ルートディレクトリ
-    port: 8000,     //ポート番号
-    livereload: true
-  });
-});
-
-//画像軽量化
-/*gulp.task('imagemin', function () {
-    gulp.src(['./assets/img/*.{png,jpg,gif}'])
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngcrush()]
+// sass compile
+gulp.task("sass", function() {
+    gulp.src("./assets/scss/*.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 6 versions', "ie 8"],
+            cascade: false
         }))
-        .pipe(gulp.dest('./assets/img/'));
-});*/
-
-
-
-gulp.task('default',['sass', 'connectDev', 'watch', 'js_compress', 'jade']);
+        .pipe(gulp.dest("./assets/css"))
+        .pipe(browser.reload({stream:true}))
+});
+ 
+gulp.task("default",['server'], function() {
+    gulp.watch(["./assets/js/*.js","!./assets/js/min/*.js"],["js"]);
+    gulp.watch("./assets/scss/*.scss",["sass"]);
+});
